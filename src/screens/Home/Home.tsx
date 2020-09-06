bc7029ac154aa823050fcf24906568049ee98230
Aleksandar <aleksandar.milakovic@igamingplatform.com>
2020-09-06 14:20:27 +0200

    axios

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Home.css";
 import { Link } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
@@ -6,12 +6,24 @@ import Grid from '@material-ui/core/Grid';
 import { ReactComponent as HomePin } from '../../assets/images/icons/pin.svg';
 import { ReactComponent as HomeWriting } from '../../assets/images/icons/writing.svg';
 import { ReactComponent as HomeCheck } from '../../assets/images/icons/check.svg';
+import axios from 'axios';
 
 interface Home {
 
 }
 
 const Home: React.FC<Home> = () => {
+
+  useEffect(() => {
+    const request = axios.get(`https://gifi.carl.rightbrain.cloud/api/v1/permitApplicationKeyValues/`, {
+      'headers': {
+        'Content-Type': 'application/json',
+        'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZ2VudF9pZCI6MSwiYXV0aG9yaXplZCI6dHJ1ZSwiZXhwIjoxNTgzODI0MDQzfQ.ZKPas_MwULoe4fgut2hlSaqEj_raWcmJm43xX7bqiSk'
+      }
+    })
+    .then(response=>console.log(response))
+  });
+
 
   return (
     <div className="home-wrapp">
